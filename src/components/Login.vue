41808007af92c7aa2214a731200cacfa4a848e12
wangxinxin <winer632@qq.com>
2024-08-06 15:01:51 +0800

    test

---
+++ src/components/Login.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+      <h1>Login</h1>
+      <form @submit.prevent="login">
+        <input type="text" v-model="username" placeholder="Username" />
+        <input type="password" v-model="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+      <p v-if="error">{{ error }}</p>
+    </div>
+  </template>
+  
+  <script>
+  import axios from "axios";
+  
+  export default {
+    data() {
+      return {
+        username: "test",
+        password: "HR1qGSAqpfrEWpZDnWBZxrh8Ll/DmFsYxjASiMqKCdqJzvb0nhAb2annRb7TeQV7RsxIqw0gd1nGyDRXm8/PpA==",
+        error: null,
+      };
+    },
+    methods: {
+      async login() {
+        try {
+          const response = await axios.post(`${process.env.VUE_APP_HTTP_PREFIX}/login`, {
+            username: this.username,
+            password: this.password,
+          });
+          console.log(response.data);
+        } catch (error) {
+          this.error = error.response ? error.response.data : "An error occurred";
+        }
+      },
+    },
+  };
+  </script>
+  
