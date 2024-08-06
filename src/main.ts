ab15e5c90288affa49703ada0e6430fc2ed37e14
wangxinxin <winer632@qq.com>
2024-08-06 15:11:39 +0800

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import Login from './components/Login.vue';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+const routes = [
+  { path: '/', component: Login },
+];
 
-import App from './App.vue'
-import router from './router'
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
 
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
